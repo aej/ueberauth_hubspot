5048806675942e70fcbe094212c268df968dffcd
Andy Jones <andy@andyjones.co>
2023-04-24 17:58:56 +0200

    Tests

--- test/ueberauth_hubspot_test.exs
+++ test/ueberauth_hubspot_test.exs
@@ -4,10 +4,6 @@ defmodule UeberauthHubspotTest do
 
   import Plug.Conn
 
-  import Ueberauth.Strategy.Helpers
-
-  doctest UeberauthHubspot
-
   defmodule SpecRouter do
     use Plug.Router
 
@@ -52,71 +48,101 @@ defmodule UeberauthHubspotTest do
     assert location =~ "state=#{conn.private[:ueberauth_state_param]}"
   end
 
-  test "handle_callback!/1 fetches the token" do
-    TestServer.add("/oauth/v1/token",
-      via: :post,
-      to: fn conn ->
-        conn
-        |> Plug.Conn.put_resp_content_type("application/json")
-        |> Plug.Conn.send_resp(
-          200,
-          Jason.encode!(%{
-            access_token: "the-access-token",
-            refresh_token: "the-refresh-token",
-            expires_in: 3600
-          })
-        )
-      end
-    )
-
-    TestServer.add("/oauth/v1/access-tokens/the-access-token",
-      via: :get,
-      to: fn conn ->
-        conn
-        |> Plug.Conn.put_resp_content_type("application/json")
-        |> Plug.Conn.send_resp(
-          200,
-          Jason.encode!(%{
-            "scopes" => ["oauth"],
-            "hub_id" => 123,
-            "app_id" => 456
-          })
-        )
-      end
-    )
-
-    Application.put_env(:ueberauth_hubspot, :base_api_url, TestServer.url())
-
-    request_conn =
-      :get
-      |> conn("/auth/hubspot", id: "foo")
-      |> SpecRouter.call(@router)
-      |> Plug.Conn.fetch_cookies()
-
-    state = request_conn.private[:ueberauth_state_param]
-    code = "some_code"
-
-    conn =
-      :get
-      |> conn("/auth/hubspot/callback",
-        id: "foo",
-        code: code,
-        state: state
+  describe "handle_callback!/1" do
+    test "fetches the token" do
+      TestServer.add("/oauth/v1/token",
+        via: :post,
+        to: fn conn ->
+          conn
+          |> Plug.Conn.put_resp_content_type("application/json")
+          |> Plug.Conn.send_resp(
+            200,
+            Jason.encode!(%{
+              access_token: "the-access-token",
+              refresh_token: "the-refresh-token",
+              expires_in: 3600
+            })
+          )
+        end
       )
-      |> Map.put(:cookies, request_conn.cookies)
-      |> Map.put(:req_cookies, request_conn.req_cookies)
-      |> Plug.Session.call(@session_options)
-      |> SpecRouter.call(@router)
 
-    auth = conn.assigns.ueberauth_auth
-    now = DateTime.utc_now() |> DateTime.add(3600) |> DateTime.to_unix()
+      TestServer.add("/oauth/v1/access-tokens/the-access-token",
+        via: :get,
+        to: fn conn ->
+          conn
+          |> Plug.Conn.put_resp_content_type("application/json")
+          |> Plug.Conn.send_resp(
+            200,
+            Jason.encode!(%{
+              "scopes" => ["oauth"],
+              "hub_id" => 123,
+              "app_id" => 456
+            })
+          )
+        end
+      )
 
-    assert auth.credentials.token == "the-access-token"
-    assert auth.credentials.refresh_token == "the-refresh-token"
-    assert auth.credentials.token_type == "Bearer"
-    assert auth.credentials.scopes == ["oauth"]
-    assert auth.credentials.expires_at == now
+      Application.put_env(:ueberauth_hubspot, :base_api_url, TestServer.url())
 
-    assert auth.extra.raw_info == %{app_id: 456, hub_id: 123}
+      request_conn =
+        :get
+        |> conn("/auth/hubspot", id: "foo")
+        |> SpecRouter.call(@router)
+        |> Plug.Conn.fetch_cookies()
+
+      state = request_conn.private[:ueberauth_state_param]
+      code = "some_code"
+
+      conn =
+        :get
+        |> conn("/auth/hubspot/callback",
+          id: "foo",
+          code: code,
+          state: state
+        )
+        |> Map.put(:cookies, request_conn.cookies)
+        |> Map.put(:req_cookies, request_conn.req_cookies)
+        |> Plug.Session.call(@session_options)
+        |> SpecRouter.call(@router)
+
+      auth = conn.assigns.ueberauth_auth
+      now = DateTime.utc_now() |> DateTime.add(3600) |> DateTime.to_unix()
+
+      assert auth.credentials.token == "the-access-token"
+      assert auth.credentials.refresh_token == "the-refresh-token"
+      assert auth.credentials.token_type == "Bearer"
+      assert auth.credentials.scopes == ["oauth"]
+      assert auth.credentials.expires_at == now
+
+      assert auth.extra.raw_info == %{app_id: 456, hub_id: 123}
+    end
+
+    test "csrf attack" do
+      request_conn =
+        :get
+        |> conn("/auth/hubspot", id: "foo")
+        |> SpecRouter.call(@router)
+        |> Plug.Conn.fetch_cookies()
+
+      code = "some_code"
+
+      conn =
+        :get
+        |> conn("/auth/hubspot/callback",
+          id: "foo",
+          code: code
+        )
+        |> Map.put(:cookies, request_conn.cookies)
+        |> Map.put(:req_cookies, request_conn.req_cookies)
+        |> Plug.Session.call(@session_options)
+        |> SpecRouter.call(@router)
+
+      assert conn.assigns.ueberauth_failure.errors == [
+               %Ueberauth.Failure.Error{
+                 message_key: "csrf_attack",
+                 message: "Cross-Site Request Forgery attack"
+               }
+             ]
+    end
   end
 end
